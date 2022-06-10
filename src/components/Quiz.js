a3969e4ebec13391d78f911ac7311bea4c4f0d8f
PJMantoss <joelptoss@gmail.com>
2022-06-10 15:00:58 +0100

    update Quiz

--- src/components/Quiz.js
+++ src/components/Quiz.js
@@ -1,5 +1,7 @@
 import React, { useState, useEffect } from 'react';
 
+const QUIZ_API_BASE_URL = 'https://api.frontendexpert.io/api/fe/quiz';
+
 const Quiz = () => {
     const [qusetions, setQuestions] = useState(null);
     // const [qusetions, setQuestions] = useState(null);
