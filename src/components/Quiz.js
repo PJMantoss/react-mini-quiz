aa80311ed2d0651a66ac653568d2fbf0730deff6
PJMantoss <joelptoss@gmail.com>
2022-06-10 14:41:44 +0100

    installed styled-component

--- src/components/Quiz.js
+++ src/components/Quiz.js
@@ -1,6 +1,9 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react';
 
 const Quiz = () => {
+    const [qusetions, setQuestions] = useState(null);
+    // const [qusetions, setQuestions] = useState(null);
+
   return (
     <div>Quiz</div>
   )
