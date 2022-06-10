a303c26f27f3d996dd5e74075a8494b6d4bbb958
PJMantoss <joelptoss@gmail.com>
2022-06-10 15:09:01 +0100

    update Quiz

--- src/components/Quiz.js
+++ src/components/Quiz.js
@@ -9,9 +9,10 @@ const Quiz = () => {
 
     useEffect(() => {
         const apiCall = async () => {
+            // fetch data from API on initial render
             const result = await fetch(QUIZ_API_BASE_URL);
             const jsonResponse = await result.json();
-            // fetch data and update state on initial render
+            // update state and re-render component
             setQuestions(jsonResponse);
         };
 
